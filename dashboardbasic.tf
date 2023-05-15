e7fa9ccfc8964911f175031d9a84950c95286103
Ananya Chowdhury <ananyachowdhury@Ananyas-Air.fritz.box>
2023-05-15 22:09:02 +0200

    adding files

--- dashboardbasic.tf
+++ dashboardbasic.tf
@@ -1,67 +1,22 @@
-terraform {
-  required_providers {
-    newrelic = {
-      source  = "newrelic/newrelic"
+
+# Super simple to load in the JSON from file. Simply use the copy as JSON icon on the UI and paste into the basic.json file. 
+
+# Note: This default dashboard JSON *will not display correctly* on your own account as the source JSON hard codes the account ID value.
+# See dash_replace.tf for version that should work in your own account :)
+
+resource "newrelic_one_dashboard_json" "basic_dashboard" {
+   json = file("${path.module}/dashboard.json")
+}
+
+#Lets tag terraform managed dashboards!
+resource "newrelic_entity_tags" "basic_dashboard" {
+    guid = newrelic_one_dashboard_json.basic_dashboard.guid
+    tag {
+        key = "terraform"
+        values = [true]
     }
-  }
 }
 
-provider "newrelic" {
-  api_key = " "
-  account_id = "3662768"
-  region = "EU"
-}
-
-resource "newrelic_one_dashboard" "tf_dashboard_as_code" {
-  title = "Observability : Dashboards as Code (Simple)"
-  editable = "read_only"
-
-  widget {
-    title         = "Transaction Response Time by Appname"
-    row           = 1
-    column        = 1
-    width         = 3
-
-    # visualization - (Required) How the widget visualizes data. 
-    # Valid values are billboard, gauge, billboard_comparison, facet_bar_chart, faceted_line_chart, facet_pie_chart, facet_table, faceted_area_chart, heatmap, attribute_sheet, single_event, histogram, funnel, raw_json, event_feed, event_table, uniques_list, line_chart, comparison_line_chart, markdown, and metric_line_chart. 
-    visualization = "faceted_line_chart"
-
-    # NRQL is a query language you can use to query the New Relic database. This document explains NRQL syntax, clauses, components, and functions.
-    # https://docs.newrelic.com/docs/query-data/nrql-new-relic-query-language/getting-started/nrql-syntax-clauses-functions
-    nrql          = "SELECT average(duration) from Transaction FACET appName TIMESERIES 1 hour SINCE 1 week ago EXTRAPOLATE"
-  }
-
-  widget {
-    title         = "Throughput Per Hour by Appname"
-    row           = 2
-    column        = 1
-    width         = 3
-
-    # visualization - (Required) How the widget visualizes data. 
-    # Valid values are billboard, gauge, billboard_comparison, facet_bar_chart, faceted_line_chart, facet_pie_chart, facet_table, faceted_area_chart, heatmap, attribute_sheet, single_event, histogram, funnel, raw_json, event_feed, event_table, uniques_list, line_chart, comparison_line_chart, markdown, and metric_line_chart. 
-    visualization = "faceted_line_chart"
-
-    # NRQL is a query language you can use to query the New Relic database. This document explains NRQL syntax, clauses, components, and functions.
-    # https://docs.newrelic.com/docs/query-data/nrql-new-relic-query-language/getting-started/nrql-syntax-clauses-functions
-    nrql          = "SELECT rate(count(*), 1 hour) FROM Transaction FACET appName TIMESERIES 1 hour SINCE 1 week ago EXTRAPOLATE"
-  }
-
-    widget {
-    title         = "Error by Appname"
-    row           = 3
-    column        = 1
-    width         = 3
-
-    # visualization - (Required) How the widget visualizes data. 
-    # Valid values are billboard, gauge, billboard_comparison, facet_bar_chart, faceted_line_chart, facet_pie_chart, facet_table, faceted_area_chart, heatmap, attribute_sheet, single_event, histogram, funnel, raw_json, event_feed, event_table, uniques_list, line_chart, comparison_line_chart, markdown, and metric_line_chart. 
-    visualization = "faceted_line_chart"
-
-    # NRQL is a query language you can use to query the New Relic database. This document explains NRQL syntax, clauses, components, and functions.
-    # https://docs.newrelic.com/docs/query-data/nrql-new-relic-query-language/getting-started/nrql-syntax-clauses-functions
-    nrql          = "SELECT percentage(count(*), WHERE error is true) FROM Transaction FACET appName TIMESERIES 1 hour SINCE 1 week ago EXTRAPOLATE"
-  }
-}
-
-output "dashboard_id" {
-  value = "${newrelic_dashboard.tf_dashboard_as_code.id}"
+output "basic_dashboard" {
+  value=newrelic_one_dashboard_json.basic_dashboard.permalink 
 }
